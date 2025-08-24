ee6ac97793e5b2e56967924c34d3c732ee069f45
Petr Maev <petr.maev94@gmail.com>
2025-08-24 22:38:07 +0300

    added serializers for users

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from tasks.serializers import TaskSerializer
+from users.models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    tasks_count = serializers.SerializerMethodField()
+    tasks_active_count = serializers.SerializerMethodField()
+    tasks = TaskSerializer(many=True, read_only=True)
+
+    def get_tasks_count(self, user):
+        return user.tasks.count()
+
+    def get_tasks_active_count(self, user):
+        return user.tasks.filter(status="work").count()
+
+    class Meta:
+        model = CustomUser
+        fields = ["id", "full_name", "email", "post", "tasks_count", "tasks_active_count", "tasks"]
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["id", "email", "full_name", "post", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
