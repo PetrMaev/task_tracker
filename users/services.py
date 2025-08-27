0c6b2a32b697deea64f9654b56b8d8c5eef2462e
Petr Maev <petr.maev94@gmail.com>
2025-08-27 22:32:02 +0300

    added custom filter for users

---
+++ users/services.py
@@ -0,0 +1,28 @@
+from django.db.models import Count, Q
+
+from tasks.models import Task
+from users.models import CustomUser
+
+
+def get_free_employees():
+    """Возвращает список словарей с наименованием задачи, сроком и наименее занятым сотрудником."""
+    employees = CustomUser.objects.annotate(tasks_active_count=Count("tasks", filter=Q(tasks__status="work")))
+    critical_tasks = Task.objects.filter(
+        status=Task.CREATED,
+        is_parent=True,
+    ).distinct()
+
+    min_tasks_count = min(emp.tasks_active_count for emp in employees)
+
+    free_employees = employees.filter(
+        Q(tasks_active_count=min_tasks_count) | Q(tasks_active_count__lte=(min_tasks_count + 2))
+    )
+
+    result = []
+
+    for task in critical_tasks:
+        for emp in free_employees:
+            if not emp.is_staff and not emp.is_director and task.is_parent:
+                result.append({"task_title": task.title, "deadline": task.deadline, "employee": emp.full_name})
+
+    return result
