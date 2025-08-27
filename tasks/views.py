a558953b99cb7c50a22b8027d334db9d8930a2d3
Petr Maev <petr.maev94@gmail.com>
2025-08-27 22:28:32 +0300

    added new permission class and task filter

--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,17 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, filters
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
+from tasks.filters import TaskFilter
 from tasks.models import Task
+from tasks.paginators import CustomPagination
 from tasks.serializers import TaskSerializer, TaskUpdateSerializer
-from users.permissions import IsOwner, IsDirector, IsExecutor
+from users.permissions import IsDirector, IsExecutor, IsOwner
 
 
 class TaskCreateAPIView(generics.CreateAPIView):
     serializer_class = TaskSerializer
-    permission_classes = [IsAuthenticated]
-    # permission_classes = [IsAuthenticated, IsDirector]
+    permission_classes = [IsAuthenticated, IsDirector]
 
     def perform_create(self, serializer):
         task = serializer.save(owner=self.request.user)  # noqa: F841
@@ -21,9 +21,9 @@ class TaskListAPIView(generics.ListAPIView):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
     permission_classes = [IsAuthenticated, IsOwner | IsExecutor]
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    filterset_fields = []
-    ordering_fields = []
+    pagination_class = CustomPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TaskFilter
 
     def get_queryset(self):
         queryset = super().get_queryset()
